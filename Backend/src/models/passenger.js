8c1aa1fdfd37bad3b118364f6fb0338119efae98
Ishmam-Hossain1 <ishmam604@gmail.com>
2025-09-04 21:47:33 +0600

    Update passenger.js

--- Backend/src/models/passenger.js
+++ Backend/src/models/passenger.js
@@ -2,6 +2,12 @@ import mongoose from "mongoose";
 
 const passengerSchema = new mongoose.Schema(
   {
+    flightId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Flight", // optional: helps with population
+      default: null
+    },
+
     name: {
       type: String,
       required: true,
@@ -21,36 +27,63 @@ const passengerSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    goldTag: {
-      type: Boolean,
-      default: false
+
+    customTag: {
+      type: String,
+      enum: ["None", "Bronze", "Silver", "Gold"],
+      default: "None",
     },
-    silverTag: {
-      type: Boolean,
-      default: false
-    },
-    bronzeTag: {
-      type: Boolean,
-      default: false
-    },
+
     flightHistory: {
-        type: Number,
+      type: Number,
     },
     notifications: {
-      type: String
+      type: [String],   
+      default: [],      
     },
-   
     flightClass: {
       type: String,
-      required: true
+      default: null
     },
     seatBooked: {
       type: Number,
-      required: true
-    }
+      default: 0
+    },
+    cargo_plane_booking_status: {
+      type: String,
+      enum: ["Not Booked", "Booked"],
+      default: "Not Booked"
+    },
+    cargo_plane_weight: {
+      type: Number,
+      default: 0, 
+    },
+    cargo_plane_flight_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CargoPlane",
+      default: null,  // Will store the booked cargo plane's ID
+    },
+    extra_luggage: {
+      type: Number,
+      default: 0,
+    },
+
   },
   { timestamps: true }
 );
+
+passengerSchema.pre("save", function (next) {
+  if (this.flightHistory <= 20) {
+    this.customTag = "None";
+  } else if (this.flightHistory <= 40) {
+    this.customTag = "Bronze";
+  } else if (this.flightHistory <= 60) {
+    this.customTag = "Silver";
+  } else if (this.flightHistory <= 80) {
+    this.customTag = "Gold";
+  }
+  next();
+});
 
 const Passenger = mongoose.model("Passenger", passengerSchema);
 
